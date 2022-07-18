d117e201b113e4d9d89f936ad937ea5b16c008e7
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-07-18 17:23:13 +0000

    various dependency and lab updates

--- lab/src/duo/main.rs
+++ lab/src/duo/main.rs
@@ -11,19 +11,21 @@ async fn main() -> Result<(), Error> {
     let mut d = Runner::new("duo");
 
     // nodes
-    let r0 = d.node("r0", "helios-1.0", 4, gb(4));
-    let r1 = d.node("r1", "helios-1.0", 4, gb(4));
+    let r0 = d.node("r0", "helios-1.1", 4, gb(4));
+    let r1 = d.node("r1", "helios-1.1", 4, gb(4));
 
     d.mount("./cargo-bay", "/opt/cargo-bay", r0)?;
     d.mount("./cargo-bay", "/opt/cargo-bay", r1)?;
 
     // links
     d.link(r0, r1);
+    d.ext_link("e1000g0", r0);
+    d.ext_link("e1000g0", r1);
 
     match run(&mut d).await? {
         RunMode::Launch => {
-            //d.exec(r0, "ipadm create-addr -t -T addrconf vioif0/v6").await?;
-            //d.exec(r1, "ipadm create-addr -t -T addrconf vioif0/v6").await?;
+            d.exec(r0, "ipadm create-addr -t -T addrconf vioif0/v6").await?;
+            d.exec(r1, "ipadm create-addr -t -T addrconf vioif0/v6").await?;
             Ok(())
         }
         _ => Ok(()),
