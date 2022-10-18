fb8f2f176c2d237d808df97c511b5c5c2bc7bcd8
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-10-17 22:11:28 -0700

    partial update message ser/des

--- bgp/src/messages.rs
+++ bgp/src/messages.rs
@@ -123,20 +123,16 @@ pub const BGP4: u8 = 4;
 /// ```text
 ///  0                   1                   2                   3
 ///  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-/// +-+-+-+-+-+-+-+-+
-/// |    Version    |
-/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-/// |     My Autonomous System      |
-/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-/// |           Hold Time           |
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-/// |                         BGP Identifier                        |
+/// |    Version    |     My Autonomous System      |   Hold Time   :
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-/// | Opt Parm Len  |
+/// :               |                BGP Identifier                 :
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-/// |                                                               |
-/// |             Optional Parameters (variable)                    |
-/// |                                                               |
+/// :               | Opt Parm Len  |     Optional Parameters       :
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// :                                                               :
+/// :             Optional Parameters (cont, variable)              :
+/// :                                                               |
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 /// ```
 ///
@@ -276,26 +272,274 @@ pub struct Tlv {
     pub value: Vec<u8>,
 }
 
+/// An update message is used to advertise feasible routes that share common
+/// path attributes to a peer, or to withdraw multiple unfeasible routes from
+/// service.
+///
+/// ```text
+///  0                   1                   2                   3
+///  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |        Witdrawn Length        |       Withdrawn Routes        :
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// :                                                               :
+/// :                Withdrawn Routes (cont, variable)              :
+/// :                                                               |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// |    Path Attribute Length      |       Path Attributes         :
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// :                                                               :
+/// :                Path Attributes (cont, variable)               :
+/// :                                                               |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// :                                                               :
+/// :       Network Layer Reachability Information (variable)       :
+/// :                                                               |
+/// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+/// ```
+///
+/// Ref: RFC 4271 §4.3
+#[derive(Debug, PartialEq, Eq)]
 pub struct UpdateMessage {
-    pub withdrawn: Vec<Lp>,
+    pub withdrawn: Vec<Prefix>,
     pub path_attributes: Vec<PathAttribute>,
-    pub nlri: Vec<Vec<u8>>,
+    pub nlri: Vec<Prefix>,
 }
 
-pub struct Lp {
+impl UpdateMessage {
+    pub fn to_wire(&self) -> Result<Vec<u8>, Error> {
+        let mut buf = Vec::new();
+
+        // withdrawn
+        let withdrawn = self.withdrawn_to_wire()?;
+        if withdrawn.len() > u16::MAX as usize {
+            return Err(Error::TooLarge);
+        }
+        let len = withdrawn.len() as u16;
+        buf.extend_from_slice(&len.to_be_bytes());
+        buf.extend_from_slice(&withdrawn);
+
+        // path attributes
+        let attrs = self.path_attrs_to_wire()?;
+        if attrs.len() > u16::MAX as usize {
+            return Err(Error::TooLarge);
+        }
+        let len = attrs.len() as u16;
+        buf.extend_from_slice(&len.to_be_bytes());
+        buf.extend_from_slice(&attrs);
+
+        // nlri
+        buf.extend_from_slice(&self.nlri_to_wire()?);
+
+        Ok(buf)
+    }
+
+    fn withdrawn_to_wire(&self) -> Result<Vec<u8>, Error> {
+        let mut buf = Vec::new();
+        for w in &self.withdrawn {
+            buf.extend_from_slice(&w.to_wire()?);
+        }
+        Ok(buf)
+    }
+
+    fn path_attrs_to_wire(&self) -> Result<Vec<u8>, Error> {
+        let mut buf = Vec::new();
+        for p in &self.path_attributes {
+            buf.extend_from_slice(&p.to_wire(
+                p.typ.flags & PathAttributeFlags::ExtendedLength as u8 != 0,
+            )?);
+        }
+        Ok(buf)
+    }
+
+    fn nlri_to_wire(&self) -> Result<Vec<u8>, Error> {
+        let mut buf = Vec::new();
+        for n in &self.nlri {
+            buf.extend_from_slice(&n.to_wire()?);
+        }
+        Ok(buf)
+    }
+
+    pub fn from_wire(buf: &[u8]) -> Result<UpdateMessage, Error> {
+        // withdrawn
+        if buf.len() < 2 {
+            return Err(Error::TooSmall);
+        }
+        let len = u16::from_be_bytes([buf[0], buf[1]]) as usize;
+        let buf = &buf[2..];
+        if buf.len() < len {
+            return Err(Error::TooSmall);
+        }
+        let withdrawn = Self::prefixes_from_wire(&buf[..len])?;
+        let buf = &buf[len..];
+
+        // path attributes
+        if buf.len() < 2 {
+            return Err(Error::TooSmall);
+        }
+        let len = u16::from_be_bytes([buf[0], buf[1]]) as usize;
+        let buf = &buf[2..];
+        if buf.len() < len {
+            return Err(Error::TooSmall);
+        }
+        let path_attributes = Self::path_attrs_from_wire(&buf[..len])?;
+        let buf = &buf[len..];
+
+        // nlri
+        let nlri = Self::prefixes_from_wire(buf)?;
+
+        Ok(UpdateMessage {
+            withdrawn,
+            path_attributes,
+            nlri,
+        })
+    }
+
+    fn prefixes_from_wire(mut buf: &[u8]) -> Result<Vec<Prefix>, Error> {
+        let mut result = Vec::new();
+        loop {
+            if buf.is_empty() {
+                break;
+            }
+            let (pfx, n) = Prefix::from_wire(buf)?;
+            result.push(pfx);
+            buf = &buf[n..];
+        }
+        Ok(result)
+    }
+
+    fn path_attrs_from_wire(
+        mut buf: &[u8],
+    ) -> Result<Vec<PathAttribute>, Error> {
+        let mut result = Vec::new();
+        loop {
+            if buf.is_empty() {
+                break;
+            }
+            let (pa, n) = PathAttribute::from_wire(buf)?;
+            result.push(pa);
+            buf = &buf[n..];
+        }
+        Ok(result)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Prefix {
     pub value: Vec<u8>,
 }
 
+impl Prefix {
+    fn to_wire(&self) -> Result<Vec<u8>, Error> {
+        if self.value.len() > u8::MAX as usize {
+            return Err(Error::TooLarge);
+        }
+        let mut buf = vec![self.value.len() as u8];
+        buf.extend_from_slice(&self.value);
+        Ok(buf)
+    }
+
+    fn from_wire(buf: &[u8]) -> Result<(Prefix, usize), Error> {
+        if buf.is_empty() {
+            return Err(Error::TooSmall);
+        }
+        let len = buf[0] as usize;
+        if buf.len() < len {
+            return Err(Error::TooSmall);
+        }
+        Ok((
+            Prefix {
+                value: buf[1..len + 1].to_owned(),
+            },
+            len + 1,
+        ))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct PathAttribute {
     pub typ: PathAttributeType,
-    pub value: Vec<u8>,
+    pub value: PathAttributeValue,
 }
 
+impl PathAttribute {
+    pub fn to_wire(&self, extended_length: bool) -> Result<Vec<u8>, Error> {
+        let mut buf = self.typ.to_wire();
+        let val = &self.value.to_wire()?;
+        if extended_length {
+            if val.len() > u16::MAX as usize {
+                return Err(Error::TooLarge);
+            }
+            let len = val.len() as u16;
+            buf.extend_from_slice(&len.to_be_bytes())
+        } else {
+            if val.len() > u8::MAX as usize {
+                return Err(Error::TooLarge);
+            }
+            buf.push(val.len() as u8);
+        }
+        buf.extend_from_slice(val);
+        Ok(buf)
+    }
+
+    fn from_wire(buf: &[u8]) -> Result<(PathAttribute, usize), Error> {
+        if buf.len() < 2 {
+            return Err(Error::TooSmall);
+        }
+        let typ = PathAttributeType::from_wire(&buf[..2])?;
+        let mut buf = &buf[2..];
+        let mut n = 2;
+        let len = if typ.flags & PathAttributeFlags::ExtendedLength as u8 != 0 {
+            if buf.len() < 2 {
+                return Err(Error::TooSmall);
+            }
+            let len = u16::from_be_bytes([buf[0], buf[1]]) as usize;
+            buf = &buf[2..];
+            n += 2;
+            len
+        } else {
+            if buf.is_empty() {
+                return Err(Error::TooSmall);
+            }
+            let len = buf[0] as usize;
+            buf = &buf[1..];
+            n += 1;
+            len
+        };
+        if buf.len() < len {
+            return Err(Error::TooSmall);
+        }
+        let buf = &buf[..len];
+        let value = PathAttributeValue::from_wire(buf, typ.type_code)?;
+        Ok((PathAttribute { typ, value }, n + len))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct PathAttributeType {
     pub flags: u8,
-    pub type_code: u8,
+    pub type_code: PathAttributeTypeCode,
 }
 
+impl PathAttributeType {
+    pub fn to_wire(&self) -> Vec<u8> {
+        vec![self.flags, self.type_code as u8]
+    }
+
+    pub fn from_wire(buf: &[u8]) -> Result<PathAttributeType, Error> {
+        Ok(PathAttributeType {
+            flags: buf[0],
+            type_code: match PathAttributeTypeCode::try_from(buf[1]) {
+                Ok(tc) => tc,
+                Err(_) => {
+                    return Err(Error::InvalidCode(buf[1]));
+                }
+            },
+        })
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 #[repr(u8)]
 pub enum PathAttributeFlags {
     Optional = 0b10000000,
@@ -304,6 +548,22 @@ pub enum PathAttributeFlags {
     ExtendedLength = 0b00010000,
 }
 
+impl std::ops::BitOr<PathAttributeFlags> for PathAttributeFlags {
+    type Output = u8;
+    fn bitor(self, other: PathAttributeFlags) -> u8 {
+        self as u8 | other as u8
+    }
+}
+
+impl std::ops::BitAnd<PathAttributeFlags> for PathAttributeFlags {
+    type Output = u8;
+    fn bitand(self, other: PathAttributeFlags) -> u8 {
+        self as u8 & other as u8
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, TryFromPrimitive)]
+#[repr(u8)]
 pub enum PathAttributeTypeCode {
     /// RFC 4271
     Origin = 1,
@@ -319,36 +579,151 @@ pub enum PathAttributeTypeCode {
     As4Aggregator = 18,
 }
 
-pub enum OriginValue {
+#[derive(Debug, PartialEq, Eq)]
+pub enum PathAttributeValue {
+    Origin(PathOrigin),
+    AsPath(Vec<AsPathSegment>),
+    NextHop(IpAddr),
+    MultiExitDisc(u32),
+    LocalPref(u32),
+    Aggregator([u8; 6]),
+    As4Path(Vec<As4PathSegment>),
+    As4Aggregator([u8; 8]),
+}
+
+impl PathAttributeValue {
+    pub fn to_wire(&self) -> Result<Vec<u8>, Error> {
+        match self {
+            Self::Origin(_) => todo!(),
+            Self::AsPath(_) => todo!(),
+            Self::NextHop(_) => todo!(),
+            Self::MultiExitDisc(_) => todo!(),
+            Self::LocalPref(_) => todo!(),
+            Self::Aggregator(_) => todo!(),
+            Self::As4Path(segments) => {
+                let mut buf = Vec::new();
+                for s in segments {
+                    buf.extend_from_slice(&s.to_wire()?);
+                }
+                Ok(buf)
+            }
+            Self::As4Aggregator(_) => todo!(),
+        }
+    }
+
+    pub fn from_wire(
+        mut buf: &[u8],
+        type_code: PathAttributeTypeCode,
+    ) -> Result<PathAttributeValue, Error> {
+        match type_code {
+            PathAttributeTypeCode::Origin => todo!(),
+            PathAttributeTypeCode::AsPath => todo!(),
+            PathAttributeTypeCode::NextHop => todo!(),
+            PathAttributeTypeCode::MultiExitDisc => todo!(),
+            PathAttributeTypeCode::LocalPref => todo!(),
+            PathAttributeTypeCode::AtomicAggregate => todo!(),
+            PathAttributeTypeCode::Aggregator => todo!(),
+            PathAttributeTypeCode::As4Path => {
+                let mut segments = Vec::new();
+                loop {
+                    if buf.is_empty() {
+                        break;
+                    }
+                    let (seg, n) = As4PathSegment::from_wire(buf)?;
+                    segments.push(seg);
+                    buf = &buf[n..];
+                }
+                Ok(PathAttributeValue::As4Path(segments))
+            }
+            PathAttributeTypeCode::As4Aggregator => todo!(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum PathOrigin {
     Igp = 0,
     Egp = 1,
     Incomplete = 2,
 }
 
-pub struct AsPathValue(Vec<AsPathSegment>);
-
+#[derive(Debug, PartialEq, Eq)]
 pub struct AsPathSegment {
+    pub typ: AsPathType,
+    pub value: Vec<u16>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct As4PathSegment {
     pub typ: AsPathType,
     pub value: Vec<u32>,
 }
 
+impl As4PathSegment {
+    pub fn to_wire(&self) -> Result<Vec<u8>, Error> {
+        if self.value.len() > u8::MAX as usize {
+            return Err(Error::TooLarge);
+        }
+        let mut buf = vec![self.typ as u8, self.value.len() as u8];
+        for v in &self.value {
+            buf.extend_from_slice(&v.to_be_bytes());
+        }
+        Ok(buf)
+    }
+
+    pub fn from_wire(mut buf: &[u8]) -> Result<(As4PathSegment, usize), Error> {
+        if buf.len() < 2 {
+            return Err(Error::TooSmall);
+        }
+        let typ = match AsPathType::try_from(buf[0]) {
+            Ok(t) => t,
+            Err(_) => {
+                return Err(Error::InvalidCode(buf[0]));
+            }
+        };
+        let mut len = buf[1] as usize;
+        len *= 4;
+        buf = &buf[2..];
+        let mut segment = As4PathSegment {
+            typ,
+            value: Vec::new(),
+        };
+        if len == 0 {
+            return Ok((segment, 2));
+        }
+
+        if buf.len() < len {
+            return Err(Error::TooSmall);
+        }
+        buf = &buf[..len];
+        loop {
+            segment
+                .value
+                .push(u32::from_be_bytes([buf[0], buf[1], buf[2], buf[3]]));
+            buf = &buf[4..];
+            if buf.is_empty() {
+                break;
+            }
+        }
+        Ok((segment, 2 + len))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, TryFromPrimitive)]
 #[repr(u8)]
 pub enum AsPathType {
     AsSet = 1,
     AsSequence = 2,
 }
 
-pub struct NextHopValue(IpAddr);
-pub struct MultiExitDisc(u32);
-pub struct LocalPref(u32);
-pub struct Aggregator([u8; 6]);
-
+#[derive(Debug, PartialEq, Eq)]
 pub struct NotificationMessage {
     pub error_code: u8,
     pub error_subcode: u8,
     pub data: Vec<u8>,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum ErrorCode {
     Header,
     Open,
@@ -358,12 +733,14 @@ pub enum ErrorCode {
     Cease,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum HeaderErrorSubcode {
     ConnectionNotSynchronized,
     BadMessageLength,
     BadMessageType,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum OpenErrorSubcode {
     UnsupportedVersionNumber,
     BadPeerAS,
@@ -373,6 +750,7 @@ pub enum OpenErrorSubcode {
     UnacceptableHoldTime,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 pub enum UpdateErrorSubcode {
     MalformedAttributeList,
     UnrecognizedWellKnownAttribute,
@@ -839,6 +1217,7 @@ pub enum CapabilityCode {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
     use pretty_hex::*;
 
     #[test]
@@ -849,7 +1228,6 @@ mod tests {
         };
 
         let buf = h0.to_wire();
-
         println!("buf: {}", buf.hex_dump());
 
         assert_eq!(
@@ -870,10 +1248,44 @@ mod tests {
         let om0 = OpenMessage::new4(395849, 0x1234, 0xaabbccdd);
 
         let buf = om0.to_wire().expect("open message to wire");
-
         println!("buf: {}", buf.hex_dump());
 
         let om1 = OpenMessage::from_wire(&buf).expect("open message from wire");
         assert_eq!(om0, om1);
+    }
+
+    #[test]
+    fn update_round_trip() {
+        let um0 = UpdateMessage {
+            withdrawn: vec![Prefix {
+                value: vec![0x20, 0x00, 0x17, 0x01, 0xc],
+            }],
+            path_attributes: vec![PathAttribute {
+                typ: PathAttributeType {
+                    flags: PathAttributeFlags::Optional
+                        | PathAttributeFlags::Partial,
+                    type_code: PathAttributeTypeCode::As4Path,
+                },
+                value: PathAttributeValue::As4Path(vec![As4PathSegment {
+                    typ: AsPathType::AsSequence,
+                    value: vec![395849, 123456, 987654, 111111],
+                }]),
+            }],
+            nlri: vec![
+                Prefix {
+                    value: vec![0x20, 0x00, 0x17, 0x01, 0xd],
+                },
+                Prefix {
+                    value: vec![0x20, 0x00, 0x17, 0x01, 0xe],
+                },
+            ],
+        };
+
+        let buf = um0.to_wire().expect("update message to wire");
+        println!("buf: {}", buf.hex_dump());
+
+        let um1 =
+            UpdateMessage::from_wire(&buf).expect("update message from wire");
+        assert_eq!(um0, um1);
     }
 }
