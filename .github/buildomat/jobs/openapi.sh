d79ca5190b7b7c3fccd81d2438bb6f1d3f149a45
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-11-11 21:41:44 -0800

    bump deps fix openapi job

--- .github/buildomat/jobs/openapi.sh
+++ .github/buildomat/jobs/openapi.sh
@@ -16,6 +16,10 @@
 #: series = "openapi"
 #: name = "ddm-admin.json.sha256.txt"
 #: from_output = "/out/ddm-admin.json.sha256.txt"
+#: [[publish]]
+#: series = "openapi"
+#: name = "bgp-admin.json"
+#: from_output = "/out/bgp-admin.json"
 #:
 
 set -o errexit
@@ -25,5 +29,9 @@ set -o xtrace
 banner copy
 pfexec mkdir -p /out
 pfexec chown "$UID" /out
+
 cp ddm-openapi/ddm-admin.json /out/ddm-admin.json
 digest -a sha256 /out/ddm-admin.json > /out/ddm-admin.json.sha256.txt
+
+cp openapi/bgp-admin.json /out/bgp-admin.json
+digest -a sha256 /out/bgp-admin.json > /out/bgp-admin.json.sha256.txt
