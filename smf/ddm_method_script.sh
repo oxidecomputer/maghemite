c976c7743fa8f3b09264ad36fbf5e441ab7d2712
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-01-21 00:48:14 +0000

    update smf service for new ddmd cli

--- smf/ddm_method_script.sh
+++ smf/ddm_method_script.sh
@@ -6,11 +6,11 @@ set -o pipefail
 export RUST_LOG=info
 
 args=(
-    "$(svcprop -c -p config/admin_port "${SMF_FMRI}")"
-    "$(svcprop -c -p config/admin_host "${SMF_FMRI}")"
-    $(svcprop -c -p config/interfaces "${SMF_FMRI}")
-    "$(svcprop -c -p config/mode "${SMF_FMRI}")"
+    --admin-port "$(svcprop -c -p config/admin_port "${SMF_FMRI}")"
+    --admin-addr "$(svcprop -c -p config/admin_host "${SMF_FMRI}")"
+    --kind "$(svcprop -c -p config/mode "${SMF_FMRI}")"
 )
+
 
 val=$(svcprop -c -p config/dendrite "${SMF_FMRI}")
 if [[ "$val" == true ]]; then
@@ -33,5 +33,10 @@ val=$(svcprop -c -p config/log "${SMF_FMRI}")
 if [[ "$val" != '""' ]]; then
     export RUST_LOG="$val"
 fi
+
+for x in $(svcprop -c -p config/interfaces "${SMF_FMRI}"); do
+    args+=( '-a' )
+    args+=( "$x" )
+done
 
 exec /opt/oxide/mg-ddm/ddmd "${args[@]}"
