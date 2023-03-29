3e05d44f0e5fcd7a23efb8b8e72ecf17a35b9e45
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-03-29 12:59:54 -0700

    cargo fmt

--- mg-ddm-verify/src/main.rs
+++ mg-ddm-verify/src/main.rs
@@ -24,7 +24,8 @@ pub struct Sled {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let sleds: Vec<Sled> = serde_json::from_str(&std::fs::read_to_string("sleds.json")?)?;
+    let sleds: Vec<Sled> =
+        serde_json::from_str(&std::fs::read_to_string("sleds.json")?)?;
 
     let all_sleds: Vec<String> = sleds.iter().map(|x| x.name.clone()).collect();
     let sled_to_ip: HashMap<String, String> = sleds
@@ -37,10 +38,13 @@ async fn main() -> Result<()> {
 
     for sled in &all_sleds {
         let response: HashMap<String, Vec<Prefix>> = serde_json::from_str(
-            &reqwest::get(format!("http://{}:8000/prefixes", &sled_to_ip[sled]))
-                .await?
-                .text()
-                .await?,
+            &reqwest::get(format!(
+                "http://{}:8000/prefixes",
+                &sled_to_ip[sled]
+            ))
+            .await?
+            .text()
+            .await?,
         )?;
 
         // expect only one next hop, I'm querying sleds that only have one
@@ -51,10 +55,13 @@ async fn main() -> Result<()> {
         sled_prefixes.insert(sled.clone(), response[next_hop].clone());
 
         let originated: Vec<Destination> = serde_json::from_str(
-            &reqwest::get(format!("http://{}:8000/originated", &sled_to_ip[sled]))
-                .await?
-                .text()
-                .await?,
+            &reqwest::get(format!(
+                "http://{}:8000/originated",
+                &sled_to_ip[sled]
+            ))
+            .await?
+            .text()
+            .await?,
         )?;
 
         sled_originated.insert(sled.clone(), originated);
@@ -62,7 +69,8 @@ async fn main() -> Result<()> {
 
     // For each sled's advertised prefixes, check that they reached every sled.
     // A direction is "missed" if an advertised prefix isn't received by a sled.
-    let mut missed_direction: HashMap<String, HashSet<String>> = HashMap::default();
+    let mut missed_direction: HashMap<String, HashSet<String>> =
+        HashMap::default();
 
     for sled in &all_sleds {
         let advertised_prefixes = &sled_originated[&sled.to_string()];
@@ -72,7 +80,8 @@ async fn main() -> Result<()> {
             let other_sled_prefixes = &sled_prefixes[&other_sled.to_string()];
 
             for prefix in advertised_prefixes {
-                if !other_sled_prefixes.iter().any(|x| x.destination == *prefix) {
+                if !other_sled_prefixes.iter().any(|x| x.destination == *prefix)
+                {
                     eprintln!(
                         "sled {} advertised {:?} but {} didn't receive it!",
                         sled, prefix, other_sled,
