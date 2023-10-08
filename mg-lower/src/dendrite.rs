68fc3fb714e3b8821c53db40ffc0f18e7ef1ad68
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-10-08 10:11:25 -0700

    mg-lower cleanup

---
+++ mg-lower/src/dendrite.rs
@@ -0,0 +1,142 @@
+use crate::MG_LOWER_TAG;
+use dpd_client::types::PortId;
+use dpd_client::Client as DpdClient;
+use libnet::{get_route, IpPrefix, Ipv4Prefix};
+use rdb::Route4ImportKey;
+use slog::{error, Logger};
+use std::hash::{Hash, Hasher};
+use std::str::FromStr;
+use std::sync::Arc;
+
+/// A wrapper around Dendrite routes so we can perform set functions over them.
+pub(crate) struct RouteHash(pub(crate) dpd_client::types::Route);
+
+impl Hash for RouteHash {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.cidr.hash(state);
+        self.0.nexthop.hash(state);
+    }
+}
+
+impl PartialEq for RouteHash {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.cidr == other.0.cidr && self.0.nexthop == other.0.nexthop
+    }
+}
+
+impl Eq for RouteHash {}
+
+/// Perform a set of route additions and deletions via the Dendrite API.
+pub(crate) fn update_dendrite<'a, I>(
+    to_add: I,
+    to_del: I,
+    dpd: &DpdClient,
+    rt: Arc<tokio::runtime::Handle>,
+    log: &Logger,
+) where
+    I: Iterator<Item = &'a RouteHash>,
+{
+    for r in to_add {
+        if let Err(e) = rt.block_on(async { dpd.route_ipv4_create(&r.0).await })
+        {
+            error!(log, "failed to create route {:?}: {}", r.0, e);
+        }
+    }
+    for r in to_del {
+        let cidr = match r.0.cidr {
+            dpd_client::Cidr::V4(cidr) => cidr,
+            _ => continue,
+        };
+        if let Err(e) =
+            rt.block_on(async { dpd.route_ipv4_delete(&cidr).await })
+        {
+            error!(log, "failed to create route {:?}: {}", r.0, e);
+        }
+    }
+}
+
+/// Translate a RIB route data structure to a Dendrite route.
+pub(crate) fn db_route_to_dendrite_route(
+    rs: Vec<Route4ImportKey>,
+    log: &Logger,
+    _dpd: &DpdClient,
+) -> Vec<dpd_client::types::Route> {
+    let mut result = Vec::new();
+
+    for r in &rs {
+        let sys_route = match get_route(IpPrefix::V4(Ipv4Prefix {
+            addr: r.nexthop,
+            mask: 32,
+        })) {
+            Ok(r) => r,
+            Err(e) => {
+                error!(log, "Unable to get route for {:?}: {}", r, e);
+                continue;
+            }
+        };
+
+        let ifname = match sys_route.ifx {
+            Some(name) => name,
+            None => {
+                error!(
+                    log,
+                    "No interface associated with route for {:?}: {:?}",
+                    r,
+                    sys_route,
+                );
+                continue;
+            }
+        };
+
+        // TODO this is gross, use link type properties rather than futzing
+        // around with strings.
+        let Some(egress_port_num) = ifname
+            .strip_prefix("tfportqsfp")
+            .and_then(|x| x.strip_suffix("_0"))
+            .map(|x| x.trim())
+            .and_then(|x| x.parse::<usize>().ok())
+        else {
+            error!(log, "expected tfportqsfp$M_$N, got {}", ifname);
+            continue;
+        };
+
+        let switch_port =
+            match PortId::from_str(&format!("qsfp{}", egress_port_num)) {
+                Ok(swp) => swp,
+                Err(e) => {
+                    error!(log, "bad port name: {e}");
+                    continue;
+                }
+            };
+
+        // TODO breakout considerations
+        let link = dpd_client::types::LinkId(0);
+
+        result.push(dpd_client::types::Route {
+            tag: MG_LOWER_TAG.into(),
+            cidr: dpd_client::Cidr::V4(dpd_client::Ipv4Cidr {
+                prefix: r.prefix.value,
+                prefix_len: r.prefix.length,
+            }),
+            switch_port,
+            link,
+            nexthop: r.nexthop.into(),
+            vid: None,
+        });
+    }
+
+    result
+}
+
+/// Create a new Dendrite/dpd client. The lower half always runs on the same
+/// host/zone as the underlying platform.
+pub(crate) fn new_dpd_client(log: &Logger) -> DpdClient {
+    let client_state = dpd_client::ClientState {
+        tag: MG_LOWER_TAG.into(),
+        log: log.clone(),
+    };
+    DpdClient::new(
+        &format!("http://localhost:{}", dpd_client::default_port()),
+        client_state,
+    )
+}
--- mg-lower/src/lib.rs
+++ mg-lower/src/lib.rs
@@ -1,70 +1,89 @@
-use dpd_client::types::PortId;
+//! This is the Maghemite external networking lower half. Its responsible for
+//! synchronizing information in a routing information base onto an underlying
+//! routing platform. The only platform currently supported is Dendrite.
+
+use crate::dendrite::{
+    db_route_to_dendrite_route, new_dpd_client, update_dendrite, RouteHash,
+};
+use crate::error::Error;
 use dpd_client::Client as DpdClient;
-use libnet::{get_route, IpPrefix, Ipv4Prefix};
-use rdb::{ChangeSet, Db, Route4ImportKey};
-use slog::{error, Logger};
+use rdb::{ChangeSet, Db};
+use slog::{error, info, Logger};
 use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
-use std::str::FromStr;
 use std::sync::mpsc::channel;
 use std::sync::Arc;
+use std::thread::sleep;
+use std::time::Duration;
 
+mod dendrite;
+mod error;
+
+/// Tag used for managing both dpd and rdb elements.
 const MG_LOWER_TAG: &str = "mg-lower";
 
+/// This is the primary entry point for the lower half. It loops forever,
+/// observing changes in the routing databse and synchronizing them to the
+/// underlying forwarding platform. The loop sets up a watcher to start
+/// receiving events, does an initial synchronization, then responds to changes
+/// moving foward. The loop runs on the calling thread, so callers are
+/// responsible for running this function in a separate thread if asynchronous
+/// execution is required.
 pub fn run(db: Db, log: Logger, rt: Arc<tokio::runtime::Handle>) {
-    let (tx, rx) = channel();
-
-    // start the db watcher first so we catch any changes that may occur while
-    // we're initializing
-    db.watch(MG_LOWER_TAG.into(), tx);
-
-    // initialize the underlying router with the current state
-    let dpd = new_dpd_client(&log);
-    let mut generation = initialize(&db, &log, &dpd, rt.clone());
-
-    // handle any changes that occur
     loop {
-        match rx.recv() {
-            Ok(change) => {
-                generation = handle_change(
-                    &db,
-                    change,
-                    &log,
-                    &dpd,
-                    generation,
-                    rt.clone(),
-                );
-            }
+        let (tx, rx) = channel();
+
+        // start the db watcher first so we catch any changes that may occur while
+        // we're initializing
+        db.watch(MG_LOWER_TAG.into(), tx);
+
+        // initialize the underlying router with the current state
+        let dpd = new_dpd_client(&log);
+        let mut generation = match initialize(&db, &log, &dpd, rt.clone()) {
+            Ok(gen) => gen,
             Err(e) => {
-                error!(log, "mg-lower watch rx: {e}");
+                error!(log, "handling change failed: {e}");
+                info!(log, "restarting sync loop in one second");
+                sleep(Duration::from_secs(1));
+                continue;
+            }
+        };
+
+        // handle any changes that occur
+        loop {
+            match rx.recv() {
+                Ok(change) => {
+                    generation = match handle_change(
+                        &db,
+                        change,
+                        &log,
+                        &dpd,
+                        generation,
+                        rt.clone(),
+                    ) {
+                        Ok(gen) => gen,
+                        Err(e) => {
+                            error!(log, "handling change failed: {e}");
+                            info!(log, "restarting sync loop");
+                            break;
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!(log, "mg-lower watch rx: {e}");
+                }
             }
         }
     }
 }
 
-struct RouteHash(dpd_client::types::Route);
-
-impl Hash for RouteHash {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.0.cidr.hash(state);
-        self.0.nexthop.hash(state);
-    }
-}
-
-impl PartialEq for RouteHash {
-    fn eq(&self, other: &Self) -> bool {
-        self.0.cidr == other.0.cidr && self.0.nexthop == other.0.nexthop
-    }
-}
-
-impl Eq for RouteHash {}
-
+/// Initialize the underlying platform with a complete set of routes from the
+/// RIB.
 fn initialize(
     db: &Db,
     log: &Logger,
     dpd: &DpdClient,
     rt: Arc<tokio::runtime::Handle>,
-) -> u64 {
+) -> Result<u64, Error> {
     let generation = db.generation();
 
     // get all imported routes from db
@@ -76,8 +95,7 @@ fn initialize(
 
     // get all routes created by mg-lower from dendrite
     let active: HashSet<RouteHash> = rt
-        .block_on(async { dpd.route_ipv4_list(None, None).await })
-        .unwrap()
+        .block_on(async { dpd.route_ipv4_list(None, None).await })?
         .items
         .iter()
         .filter(|x| x.tag == MG_LOWER_TAG)
@@ -90,109 +108,10 @@ fn initialize(
 
     update_dendrite(to_add, to_del, dpd, rt, log);
 
-    generation
+    Ok(generation)
 }
 
-fn update_dendrite<'a, I>(
-    to_add: I,
-    to_del: I,
-    dpd: &DpdClient,
-    rt: Arc<tokio::runtime::Handle>,
-    log: &Logger,
-) where
-    I: Iterator<Item = &'a RouteHash>,
-{
-    for r in to_add {
-        if let Err(e) = rt.block_on(async { dpd.route_ipv4_create(&r.0).await })
-        {
-            error!(log, "failed to create route {:?}: {}", r.0, e);
-        }
-    }
-    for r in to_del {
-        let cidr = match r.0.cidr {
-            dpd_client::Cidr::V4(cidr) => cidr,
-            _ => continue,
-        };
-        if let Err(e) =
-            rt.block_on(async { dpd.route_ipv4_delete(&cidr).await })
-        {
-            error!(log, "failed to create route {:?}: {}", r.0, e);
-        }
-    }
-}
-
-fn db_route_to_dendrite_route(
-    rs: Vec<Route4ImportKey>,
-    log: &Logger,
-    _dpd: &DpdClient,
-) -> Vec<dpd_client::types::Route> {
-    let mut result = Vec::new();
-
-    for r in &rs {
-        let sys_route = match get_route(IpPrefix::V4(Ipv4Prefix {
-            addr: r.nexthop,
-            mask: 32,
-        })) {
-            Ok(r) => r,
-            Err(e) => {
-                error!(log, "Unable to get route for {:?}: {}", r, e);
-                continue;
-            }
-        };
-
-        let ifname = match sys_route.ifx {
-            Some(name) => name,
-            None => {
-                error!(
-                    log,
-                    "No interface associated with route for {:?}: {:?}",
-                    r,
-                    sys_route,
-                );
-                continue;
-            }
-        };
-
-        // TODO this is gross, use link type properties rather than futzing
-        // around with strings.
-        let Some(egress_port_num) = ifname
-            .strip_prefix("tfportqsfp")
-            .and_then(|x| x.strip_suffix("_0"))
-            .map(|x| x.trim())
-            .and_then(|x| x.parse::<usize>().ok())
-        else {
-            error!(log, "expected tfportqsfp$M_$N, got {}", ifname);
-            continue;
-        };
-
-        let switch_port =
-            match PortId::from_str(&format!("qsfp{}", egress_port_num)) {
-                Ok(swp) => swp,
-                Err(e) => {
-                    error!(log, "bad port name: {e}");
-                    continue;
-                }
-            };
-
-        // TODO breakout considerations
-        let link = dpd_client::types::LinkId(0);
-
-        result.push(dpd_client::types::Route {
-            tag: MG_LOWER_TAG.into(),
-            cidr: dpd_client::Cidr::V4(dpd_client::Ipv4Cidr {
-                prefix: r.prefix.value,
-                prefix_len: r.prefix.length,
-            }),
-            switch_port,
-            link,
-            nexthop: r.nexthop.into(),
-            vid: None,
-        });
-    }
-
-    result
-}
-
+/// Synchronize a change set from the RIB to the underlying platform.
 fn handle_change(
     db: &Db,
     change: ChangeSet,
@@ -200,11 +119,10 @@ fn handle_change(
     dpd: &DpdClient,
     generation: u64,
     rt: Arc<tokio::runtime::Handle>,
-) -> u64 {
+) -> Result<u64, Error> {
     if change.generation > generation + 1 {
         return initialize(db, log, dpd, rt.clone());
     }
-    //TODO avoid this translation
     let to_add = change.import.added.clone().into_iter().collect();
     let to_add: HashSet<RouteHash> =
         db_route_to_dendrite_route(to_add, log, dpd)
@@ -212,7 +130,6 @@ fn handle_change(
             .map(|x| RouteHash(x.clone()))
             .collect();
 
-    //TODO avoid this translation
     let to_del = change.import.removed.clone().into_iter().collect();
     let to_del: HashSet<RouteHash> =
         db_route_to_dendrite_route(to_del, log, dpd)
@@ -222,16 +139,5 @@ fn handle_change(
 
     update_dendrite(to_add.iter(), to_del.iter(), dpd, rt.clone(), log);
 
-    change.generation
-}
-
-fn new_dpd_client(log: &Logger) -> DpdClient {
-    let client_state = dpd_client::ClientState {
-        tag: MG_LOWER_TAG.into(),
-        log: log.clone(),
-    };
-    DpdClient::new(
-        &format!("http://localhost:{}", dpd_client::default_port()),
-        client_state,
-    )
+    Ok(change.generation)
 }
