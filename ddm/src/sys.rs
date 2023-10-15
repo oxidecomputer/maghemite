666180734f45766512907896a7512ac017e388ba
Nils Nieuwejaar <nils@oxidecomputer.com>
2023-10-15 21:27:28 +0000

    adapted to new dpd-client::PortId

--- ddm/src/sys.rs
+++ ddm/src/sys.rs
@@ -323,23 +323,15 @@ pub fn get_routes_dendrite(
             Cidr::V6(cidr) => (cidr.prefix.into(), cidr.prefix_len),
             _ => continue,
         };
-        let parts: Vec<&str> = r.switch_port.split(':').collect();
-        if parts.is_empty() {
-            return Err(format!(
-                "expected port format M:N, got {}",
-                r.switch_port.as_str()
-            ));
-        }
-        let egress_port = match parts[0].parse::<u16>() {
-            Ok(n) => n,
-            Err(e) => {
-                return Err(format!(
-                    "expected port format M:N, got {}: {}",
-                    r.switch_port.as_str(),
-                    e,
-                ))
-            }
+
+        let egress_port = match r.switch_port {
+            types::PortId::Rear(port) => port
+                .to_string()
+                .parse::<u16>()
+                .expect("RearPort(n) is a u8, so should always parse"),
+            _ => continue,
         };
+
         result.push(Route {
             dest,
             prefix_len,
