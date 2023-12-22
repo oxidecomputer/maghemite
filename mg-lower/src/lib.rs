92d176631c6755fb43d6b43fc991c66dadb93812
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-12-21 18:49:11 -0800

    prep for open source release

--- mg-lower/src/lib.rs
+++ mg-lower/src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! This is the Maghemite external networking lower half. Its responsible for
 //! synchronizing information in a routing information base onto an underlying
 //! routing platform. The only platform currently supported is Dendrite.
