9cefc93888607000469c062652364ba1ce5bd54e
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2024-03-22 08:58:06 -0700

    cleanup

--- ddm/src/admin.rs
+++ ddm/src/admin.rs
@@ -104,7 +104,53 @@ pub fn handler(
     Ok(())
 }
 
-/// Status of a DDM peer with state expressed as durations.
+/// Status of a DDM peer.
+#[derive(
+    Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema,
+)]
+#[serde(rename = "PeerStatus")]
+pub enum PeerStatusV1 {
+    NoContact,
+    Active,
+    Expired,
+}
+
+// Translate internal peer status which is based on instants, to API
+// representation which is based on durations.
+impl From<crate::db::PeerStatus> for PeerStatusV1 {
+    fn from(value: crate::db::PeerStatus) -> Self {
+        match value {
+            crate::db::PeerStatus::NoContact => Self::NoContact,
+            crate::db::PeerStatus::Init(_)
+            | crate::db::PeerStatus::Solicit(_)
+            | crate::db::PeerStatus::Exchange(_) => Self::Active,
+            crate::db::PeerStatus::Expired(_) => Self::Expired,
+        }
+    }
+}
+
+/// Information about a DDM peer.
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize, JsonSchema)]
+#[serde(rename = "PeerInfo")]
+pub struct PeerInfoV1 {
+    pub status: PeerStatusV1,
+    pub addr: Ipv6Addr,
+    pub host: String,
+    pub kind: RouterKind,
+}
+
+impl From<crate::db::PeerInfo> for PeerInfoV1 {
+    fn from(value: crate::db::PeerInfo) -> Self {
+        Self {
+            status: value.status.into(),
+            addr: value.addr,
+            host: value.host,
+            kind: value.kind,
+        }
+    }
+}
+
+/// Status of a DDM peer. Includes how long the current status has been active.
 #[derive(
     Debug, Copy, Clone, PartialEq, Eq, Deserialize, Serialize, JsonSchema,
 )]
@@ -148,49 +194,6 @@ pub struct PeerInfoV2 {
     pub kind: RouterKind,
 }
 
-#[derive(
-    Debug, Copy, Clone, PartialEq, Eq, Serialize, Deserialize, JsonSchema,
-)]
-pub enum PeerStatus {
-    NoContact,
-    Active,
-    Expired,
-}
-
-// Translate internal peer status which is based on instants, to API
-// representation which is based on durations.
-impl From<crate::db::PeerStatus> for PeerStatus {
-    fn from(value: crate::db::PeerStatus) -> Self {
-        match value {
-            crate::db::PeerStatus::NoContact => Self::NoContact,
-            crate::db::PeerStatus::Init(_)
-            | crate::db::PeerStatus::Solicit(_)
-            | crate::db::PeerStatus::Exchange(_) => Self::Active,
-            crate::db::PeerStatus::Expired(_) => Self::Expired,
-        }
-    }
-}
-
-impl From<crate::db::PeerInfo> for PeerInfo {
-    fn from(value: crate::db::PeerInfo) -> Self {
-        Self {
-            status: value.status.into(),
-            addr: value.addr,
-            host: value.host,
-            kind: value.kind,
-        }
-    }
-}
-
-/// Information about a DDM peer.
-#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize, JsonSchema)]
-pub struct PeerInfo {
-    pub status: PeerStatus,
-    pub addr: Ipv6Addr,
-    pub host: String,
-    pub kind: RouterKind,
-}
-
 impl From<crate::db::PeerInfo> for PeerInfoV2 {
     fn from(value: crate::db::PeerInfo) -> Self {
         Self {
@@ -205,7 +208,7 @@ impl From<crate::db::PeerInfo> for PeerInfoV2 {
 #[endpoint { method = GET, path = "/peers" }]
 async fn get_peers(
     ctx: RequestContext<Arc<Mutex<HandlerContext>>>,
-) -> Result<HttpResponseOk<HashMap<u32, PeerInfo>>, HttpError> {
+) -> Result<HttpResponseOk<HashMap<u32, PeerInfoV1>>, HttpError> {
     let ctx = ctx.context().lock().unwrap();
     let peers = ctx
         .db
