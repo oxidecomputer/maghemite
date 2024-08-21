77bd87a9989c6f3752b580c8fce2d245715c8ce9
Trey Aspelund <trey@oxidecomputer.com>
2024-08-20 23:07:58 -0600

    Disable clippy::type_complexity checks for bgp_admin.rs
    Until we know how type aliases will work with progenitor and dropshot,
    we will disable this clippy lint.
    
    Signed-off-by: Trey Aspelund <trey@oxidecomputer.com>

--- mgd/src/bgp_admin.rs
+++ mgd/src/bgp_admin.rs
@@ -2,6 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+#![allow(clippy::type_complexity)]
 use crate::bgp_param as resource;
 use crate::{admin::HandlerContext, bgp_param::*, error::Error, register};
 use bgp::router::LoadPolicyError;
