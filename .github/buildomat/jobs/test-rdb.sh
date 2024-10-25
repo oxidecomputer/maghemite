3ae53bb804f4c647abdebef3d16f79303de87d75
Trey Aspelund <trey@oxidecomputer.com>
2024-10-25 10:48:23 -0700

    Enable rdb tests in CI
    Signed-off-by: Trey Aspelund <trey@oxidecomputer.com>

---
+++ .github/buildomat/jobs/test-rdb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#:
+#: name = "test-rdb"
+#: variety = "basic"
+#: target = "helios-2.0"
+#: rust_toolchain = "stable"
+#: output_rules = [
+#:   "/work/*.log",
+#: ]
+#: access_repos = [
+#:   "oxidecomputer/dendrite",
+#: ]
+#:
+
+set -x
+set -e
+
+cargo --version
+rustc --version
+
+cargo install cargo-nextest
+
+pushd rdb
+
+cargo nextest run
+cp *.log /work/
