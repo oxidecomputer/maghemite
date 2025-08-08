560eaa724f8949c59474fa66f36c60612683b251
Trey Aspelund <trey@oxidecomputer.com>
2025-08-07 19:51:27 -0600

    Add buildomat job for mgadm
    Signed-off-by: Trey Aspelund <trey@oxidecomputer.com>

---
+++ .github/buildomat/jobs/test-mgadm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#:
+#: name = "test-rdb"
+#: variety = "basic"
+#: target = "helios-2.0"
+#: rust_toolchain = "stable"
+#: output_rules = [
+#:   "/work/*.log",
+#: ]
+#: access_repos = [
+#:   "oxidecomputer/dendrite",
+#: ]
+#:
+
+set -x
+set -e
+
+# NOTE: This version should be in sync with the recommended version in
+# .config/nextest.toml. (Maybe build an automated way to pull the recommended
+# version in the future.)
+NEXTEST_VERSION='0.9.97'
+PLATFORM='illumos'
+
+cargo --version
+rustc --version
+curl -sSfL --retry 10 https://get.nexte.st/"$NEXTEST_VERSION"/"$PLATFORM" | gunzip | tar -xvf - -C ~/.cargo/bin
+
+pushd mgadm
+
+cargo nextest run
+cp *.log /work/
