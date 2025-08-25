47378e5203d3660e33421b9079cec86f335905f7
Trey Aspelund <trey@oxidecomputer.com>
2025-08-25 17:09:29 -0400

    bgp: refactor tests and add support for TCP
    Refactors the existing test functions to be helpers/providers of
    a topology and test case. Actual tests as noted by cargo will now
    be consumers of these providers, and will provide test parameters
    (currently passive_tcp_establishment) and unique sockaddrs to
    ensure tests can run in parallel regardless of whether the test
    cases use TCP or Channels for their transport.
    
    Signed-off-by: Trey Aspelund <trey@oxidecomputer.com>

--- bgp/src/test.rs
+++ bgp/src/test.rs
@@ -2,21 +2,43 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::config::{PeerConfig, RouterConfig};
-use crate::connection_channel::{BgpConnectionChannel, BgpListenerChannel};
-use crate::session::{FsmStateKind, SessionInfo};
-use mg_common::{
-    cidr, ip, parse, sockaddr, wait_for, wait_for_eq, wait_for_neq,
+use crate::{
+    config::{PeerConfig, RouterConfig},
+    connection::{BgpConnection, BgpListener},
+    connection_channel::{BgpConnectionChannel, BgpListenerChannel},
+    connection_tcp::{BgpConnectionTcp, BgpListenerTcp},
+    dispatcher::Dispatcher,
+    router::Router,
+    session::{FsmStateKind, SessionInfo},
 };
+use mg_common::*;
 use rdb::{Asn, Prefix};
-use std::collections::BTreeMap;
-use std::sync::mpsc::channel;
-use std::sync::{Arc, Mutex};
-use std::thread::spawn;
+use std::{
+    collections::BTreeMap,
+    net::SocketAddr,
+    sync::{mpsc::channel, Arc, Mutex},
+    thread::spawn,
+};
 
-type Router = crate::router::Router<BgpConnectionChannel>;
-type Dispatcher = crate::dispatcher::Dispatcher<BgpConnectionChannel>;
-type FsmEvent = crate::session::FsmEvent<BgpConnectionChannel>;
+struct TestRouter<Cnx: BgpConnection> {
+    router: Arc<Router<Cnx>>,
+    dispatcher: Arc<Dispatcher<Cnx>>,
+}
+
+impl<Cnx: BgpConnection + 'static> TestRouter<Cnx> {
+    fn shutdown(&self) {
+        self.router.shutdown();
+        self.dispatcher.shutdown();
+    }
+
+    fn run<Listener: BgpListener<Cnx> + 'static>(&self) {
+        self.router.run();
+        let d = self.dispatcher.clone();
+        spawn(move || {
+            d.run::<Listener>();
+        });
+    }
+}
 
 // This test effectively does the following:
 // 1. Sets up a basic pair of routers, r1 and r2
@@ -29,33 +51,57 @@ type FsmEvent = crate::session::FsmEvent<BgpConnectionChannel>;
 //    or Connect (active tcp establishment)
 // 7. Restarts r2
 // 8. Ensures the BGP session between r1 and r2 moves back into Established
-fn basic_peering_helper(passive: bool) {
-    let test_str = match passive {
-        true => "basic_peering_passive",
-        false => "basic_peering_active",
+fn basic_peering<
+    Cnx: BgpConnection + 'static,
+    Listener: BgpListener<Cnx> + 'static,
+>(
+    passive: bool,
+    r1_addr: SocketAddr,
+    r2_addr: SocketAddr,
+    r1_peer: SocketAddr,
+    r2_peer: SocketAddr,
+) {
+    let is_tcp = std::any::type_name::<Cnx>().contains("Tcp");
+    let test_str = match (passive, is_tcp) {
+        (true, true) => "basic_peering_passive_tcp",
+        (false, true) => "basic_peering_active_tcp",
+        (true, false) => "basic_peering_passive",
+        (false, false) => "basic_peering_active",
     };
 
-    let (r1, d1, r2, d2) = two_router_test_setup(
+    let (r1, r2) = two_router_test_setup::<Cnx, Listener>(
         test_str,
         Some(SessionInfo {
             passive_tcp_establishment: passive,
             ..Default::default()
         }),
         None,
+        r1_addr,
+        r2_addr,
+        r1_peer,
+        r2_peer,
     );
 
-    let r1_session = r1.get_session(ip!("2.0.0.1")).expect("get session one");
-    let r2_session = r2.get_session(ip!("1.0.0.1")).expect("get session two");
+    // Generate consistent IPs based on the addresses we used for session lookup
+    let (r1_peer_ip, r2_peer_ip) = if is_tcp {
+        (ip!("127.0.0.1"), ip!("127.0.0.1")) // TCP can use same IP with different ports
+    } else {
+        // Channel-based: extract IPs from the peer host addresses we used
+        (r1_peer.ip(), r2_peer.ip()) // r1 looks for r2, r2 looks for r1
+    };
+
+    let r1_session =
+        r1.router.get_session(r1_peer_ip).expect("get session one");
+    let r2_session =
+        r2.router.get_session(r2_peer_ip).expect("get session two");
 
     // Give peer sessions a few seconds and ensure we have reached the
     // established state on both sides.
-
     wait_for_eq!(r1_session.state(), FsmStateKind::Established);
     wait_for_eq!(r2_session.state(), FsmStateKind::Established);
 
     // Shut down r2 and ensure that r2's peer session has gone back to idle.
     r2.shutdown();
-    d2.shutdown();
     wait_for_eq!(
         r2_session.state(),
         FsmStateKind::Idle,
@@ -82,12 +128,9 @@ fn basic_peering_helper(passive: bool) {
         );
     }
 
-    r2.run();
-    let d2_clone = d2.clone();
-    spawn(move || {
-        d2_clone.run::<BgpListenerChannel>();
-    });
-    r2.send_event(FsmEvent::ManualStart)
+    r2.run::<Listener>();
+    r2.router
+        .send_event(crate::session::FsmEvent::ManualStart)
         .expect("manual start session two");
 
     wait_for_eq!(
@@ -103,17 +146,7 @@ fn basic_peering_helper(passive: bool) {
 
     // Clean up properly
     r1.shutdown();
-    d1.shutdown();
-}
-
-#[test]
-fn test_basic_peering_passive() {
-    basic_peering_helper(true);
-}
-
-#[test]
-fn test_basic_peering_active() {
-    basic_peering_helper(false);
+    r2.shutdown();
 }
 
 // This test does the following:
@@ -125,50 +158,84 @@ fn test_basic_peering_active() {
 // 6. Shuts down r1
 // 7. Ensures the BGP FSM moves out of Established on both r1 and r2
 // 8. Ensures r2 has successfully uninstalled the implicitly withdrawn prefix
-#[test]
-fn test_basic_update() {
-    let (r1, d1, r2, d2) = two_router_test_setup("basic_update", None, None);
+fn basic_update<
+    Cnx: BgpConnection + 'static,
+    Listener: BgpListener<Cnx> + 'static,
+>(
+    r1_addr: SocketAddr,
+    r2_addr: SocketAddr,
+    r1_peer: SocketAddr,
+    r2_peer: SocketAddr,
+) {
+    let is_tcp = std::any::type_name::<Cnx>().contains("Tcp");
+    let test_name = if is_tcp {
+        "basic_update_tcp"
+    } else {
+        "basic_update"
+    };
+    let (r1, r2) = two_router_test_setup::<Cnx, Listener>(
+        test_name, None, None, r1_addr, r2_addr, r1_peer, r2_peer,
+    );
 
     // originate a prefix
-    r1.create_origin4(vec![ip!("1.2.3.0/24")])
+    r1.router
+        .create_origin4(vec![ip!("1.2.3.0/24")])
         .expect("originate");
 
+    // Generate consistent IPs based on the addresses we used for session lookup
+    let (r1_peer_ip, r2_peer_ip) = if is_tcp {
+        (ip!("127.0.0.1"), ip!("127.0.0.1")) // TCP can use same IP with different ports
+    } else {
+        // Channel-based: extract IPs from the peer host addresses we used
+        (r1_peer.ip(), r2_peer.ip()) // r1 looks for r2, r2 looks for r1
+    };
+
     // once we reach established the originated routes should have propagated
-    let r1_session = r1.get_session(ip!("2.0.0.1")).expect("get session one");
-    let r2_session = r2.get_session(ip!("1.0.0.1")).expect("get session two");
+    let r1_session =
+        r1.router.get_session(r1_peer_ip).expect("get session one");
+    let r2_session =
+        r2.router.get_session(r2_peer_ip).expect("get session two");
     wait_for_eq!(r1_session.state(), FsmStateKind::Established);
     wait_for_eq!(r2_session.state(), FsmStateKind::Established);
 
     let prefix = Prefix::V4(cidr!("1.2.3.0/24"));
 
-    wait_for_eq!(r2.db.get_prefix_paths(&prefix).is_empty(), false);
+    wait_for_eq!(r2.router.db.get_prefix_paths(&prefix).is_empty(), false);
 
     // shut down r1 and ensure that the prefixes are withdrawn from r2 on
     // session timeout.
     r1.shutdown();
-    d1.shutdown();
     wait_for_neq!(
         r1_session.state(),
         FsmStateKind::Established,
-        "r2 state should NOT be established after shutdown of r1"
+        "r1 state should NOT be established after being shutdown"
     );
     wait_for_neq!(
         r2_session.state(),
         FsmStateKind::Established,
-        "r1 state should NOT be established after being shutdown"
+        "r2 state should NOT be established after shutdown of r1"
     );
-    wait_for_eq!(r2.db.get_prefix_paths(&prefix).is_empty(), true);
+    wait_for_eq!(r2.router.db.get_prefix_paths(&prefix).is_empty(), true);
 
     // Clean up properly
     r2.shutdown();
-    d2.shutdown();
 }
 
-fn two_router_test_setup(
+fn two_router_test_setup<
+    Cnx: BgpConnection + 'static,
+    Listener: BgpListener<Cnx> + 'static,
+>(
     name: &str,
     r1_info: Option<SessionInfo>,
     r2_info: Option<SessionInfo>,
-) -> (Arc<Router>, Arc<Dispatcher>, Arc<Router>, Arc<Dispatcher>) {
+    r1_addr: SocketAddr,
+    r2_addr: SocketAddr,
+    r1_peer: SocketAddr,
+    r2_peer: SocketAddr,
+) -> (TestRouter<Cnx>, TestRouter<Cnx>)
+where
+    Cnx: Clone + Send,
+{
     let log = mg_common::log::init_file_logger(&format!("r1.{name}.log"));
 
     std::fs::create_dir_all("/tmp").expect("create tmp dir");
@@ -180,15 +247,14 @@ fn two_router_test_setup(
     let db = rdb::Db::new(&db_path, log.clone()).expect("create db");
 
     let a2s1 = Arc::new(Mutex::new(BTreeMap::new()));
-    let d1 =
-        Arc::new(crate::dispatcher::Dispatcher::<BgpConnectionChannel>::new(
-            a2s1.clone(),
-            "1.0.0.1:179".into(),
-            log.clone(),
-        ));
+    let d1 = Arc::new(Dispatcher::<Cnx>::new(
+        a2s1.clone(),
+        r1_addr.to_string(),
+        log.clone(),
+    ));
 
     let (r1_event_tx, event_rx) = channel();
-    let r1 = Arc::new(Router::new(
+    let r1_router = Arc::new(Router::<Cnx>::new(
         RouterConfig {
             asn: Asn::FourOctet(4200000001),
             id: 1,
@@ -198,33 +264,39 @@ fn two_router_test_setup(
         a2s1.clone(),
     ));
 
-    r1.run();
+    r1_router.run();
     let d = d1.clone();
     spawn(move || {
-        d.run::<BgpListenerChannel>();
+        d.run::<Listener>();
     });
 
-    r1.new_session(
-        PeerConfig {
-            name: "r2".into(),
-            host: sockaddr!("2.0.0.1:179"),
-            hold_time: 6,
-            idle_hold_time: 6,
-            delay_open: 0,
-            connect_retry: 1,
-            keepalive: 3,
-            resolution: 100,
-        },
-        sockaddr!("1.0.0.1:179"),
-        r1_event_tx.clone(),
-        event_rx,
-        r1_info.unwrap_or_default(),
-    )
-    .expect("new session on router one");
+    r1_router
+        .new_session(
+            PeerConfig {
+                name: "r2".into(),
+                host: r1_peer,
+                hold_time: 6,
+                idle_hold_time: 6,
+                delay_open: 0,
+                connect_retry: 1,
+                keepalive: 3,
+                resolution: 100,
+            },
+            r1_addr,
+            r1_event_tx.clone(),
+            event_rx,
+            r1_info.unwrap_or_default(),
+        )
+        .expect("new session on router one");
 
     r1_event_tx
-        .send(FsmEvent::ManualStart)
+        .send(crate::session::FsmEvent::ManualStart)
         .expect("session manual start on router one");
+
+    let r1 = TestRouter {
+        router: r1_router,
+        dispatcher: d1,
+    };
 
     // Router 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -236,16 +308,15 @@ fn two_router_test_setup(
         .expect("create datastore for router 2");
 
     let a2s2 = Arc::new(Mutex::new(BTreeMap::new()));
-    let d2 =
-        Arc::new(crate::dispatcher::Dispatcher::<BgpConnectionChannel>::new(
-            a2s2.clone(),
-            "2.0.0.1:179".into(),
-            log.clone(),
-        ));
+    let d2 = Arc::new(Dispatcher::<Cnx>::new(
+        a2s2.clone(),
+        r2_addr.to_string(),
+        log.clone(),
+    ));
 
     let (r2_event_tx, event_rx) = channel();
 
-    let r2 = Arc::new(Router::new(
+    let r2_router = Arc::new(Router::<Cnx>::new(
         RouterConfig {
             asn: Asn::FourOctet(4200000002),
             id: 2,
@@ -255,33 +326,124 @@ fn two_router_test_setup(
         a2s2.clone(),
     ));
 
-    r2.run();
+    r2_router.run();
     let d = d2.clone();
     spawn(move || {
-        d.run::<BgpListenerChannel>();
+        d.run::<Listener>();
     });
 
-    r2.new_session(
-        PeerConfig {
-            name: "r1".into(),
-            host: sockaddr!("1.0.0.1:179"),
-            hold_time: 6,
-            idle_hold_time: 6,
-            delay_open: 0,
-            connect_retry: 1,
-            keepalive: 3,
-            resolution: 100,
-        },
-        sockaddr!("2.0.0.1:179"),
-        r2_event_tx.clone(),
-        event_rx,
-        r2_info.unwrap_or_default(),
-    )
-    .expect("new session on router two");
+    r2_router
+        .new_session(
+            PeerConfig {
+                name: "r1".into(),
+                host: r2_peer,
+                hold_time: 6,
+                idle_hold_time: 6,
+                delay_open: 0,
+                connect_retry: 1,
+                keepalive: 3,
+                resolution: 100,
+            },
+            r2_addr,
+            r2_event_tx.clone(),
+            event_rx,
+            r2_info.unwrap_or_default(),
+        )
+        .expect("new session on router two");
 
     r2_event_tx
-        .send(FsmEvent::ManualStart)
+        .send(crate::session::FsmEvent::ManualStart)
         .expect("start session on router two");
 
-    (r1, d1, r2, d2)
+    let r2 = TestRouter {
+        router: r2_router,
+        dispatcher: d2,
+    };
+
+    (r1, r2)
+}
+
+// In order to facilitate test cases being run in parallel, sockaddrs must
+// be unique, although the reasoning is different for Channels and TCP.
+//
+// Channels:
+// The current BgpConnectionChannel implementation does not have a single
+// listener that splits off individual connections as they are accept()'d. It
+// uses the Listener's SocketAddr as the key in a HashMap to coordinate the
+// exchange of the local and remote halves of a duplex channel, therefore a
+// Listener is implicitly coupled to a single channel. Given this, each
+// Channel-based test case will supply unique IPs. The port provided here is
+// pinned to 179 (BGP) as a convention, but is not strictly necessary.
+//
+// TCP:
+// A single TcpListener can accept() multiple connections, as you'd expect from
+// a typical OS TCP/IP implementation.  However, since the test has multiple
+// logical routers making use of the same TCP/IP stack, each listener must
+// bind() to a different port to avoid collisions (i.e. EADDRINUSE). To avoid
+// the need for IP management, we use the unspecified address (0.0.0.0) as the
+// local listener, and the loopback address (127.0.0.1) as the remote peer.
+// Port numbers supplied here must be unique per logical router.
+
+#[test]
+fn test_basic_peering_passive() {
+    basic_peering::<BgpConnectionChannel, BgpListenerChannel>(
+        true,
+        sockaddr!("10.0.0.1:179"),
+        sockaddr!("10.0.0.2:179"),
+        sockaddr!("10.0.0.2:179"),
+        sockaddr!("10.0.0.1:179"),
+    )
+}
+
+#[test]
+fn test_basic_peering_active() {
+    basic_peering::<BgpConnectionChannel, BgpListenerChannel>(
+        false,
+        sockaddr!("11.0.0.1:179"),
+        sockaddr!("11.0.0.2:179"),
+        sockaddr!("11.0.0.2:179"),
+        sockaddr!("11.0.0.1:179"),
+    )
+}
+
+#[test]
+fn test_basic_peering_passive_tcp() {
+    basic_peering::<BgpConnectionTcp, BgpListenerTcp>(
+        true,
+        sockaddr!("0.0.0.0:20000"),
+        sockaddr!("0.0.0.0:20001"),
+        sockaddr!("127.0.0.1:20001"),
+        sockaddr!("127.0.0.1:20000"),
+    )
+}
+
+#[test]
+fn test_basic_peering_active_tcp() {
+    basic_peering::<BgpConnectionTcp, BgpListenerTcp>(
+        false,
+        sockaddr!("0.0.0.0:20010"),
+        sockaddr!("0.0.0.0:20011"),
+        sockaddr!("127.0.0.1:20011"),
+        sockaddr!("127.0.0.1:20010"),
+    )
+}
+
+#[test]
+fn test_basic_update() {
+    basic_update::<BgpConnectionChannel, BgpListenerChannel>(
+        sockaddr!("12.0.0.1:179"),
+        sockaddr!("12.0.0.2:179"),
+        sockaddr!("12.0.0.2:179"),
+        sockaddr!("12.0.0.1:179"),
+    )
+}
+
+#[test]
+fn test_basic_update_tcp() {
+    basic_update::<BgpConnectionTcp, BgpListenerTcp>(
+        sockaddr!("0.0.0.0:20020"),
+        sockaddr!("0.0.0.0:20021"),
+        sockaddr!("127.0.0.1:20021"),
+        sockaddr!("127.0.0.1:20020"),
+    )
 }
