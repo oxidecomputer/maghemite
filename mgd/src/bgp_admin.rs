23b66f65268366e90c29168dbad711e820a94a6e
Trey Aspelund <trey@oxidecomputer.com>
2026-02-13 21:11:44 -0700

    Stop walking RIBs for AFIs that weren't negotiated
    There were multiple places where we kicked off a RIB walker as a result
    of an event pertaining to a BGP peer which unconditionally locked and
    walked the RIBs for both IPv4 and IPv6. Update these spots to only walk
    the RIBs for address-families that have been negotiated with the peer.

--- mgd/src/bgp_admin.rs
+++ mgd/src/bgp_admin.rs
@@ -1953,7 +1953,8 @@ pub(crate) mod helpers {
     ) -> Result<HttpResponseDeleted, Error> {
         bgp_log!(ctx.log, info, "remove neighbor (addr {addr}, asn {asn})");
 
-        ctx.db.remove_all_prefixes_from_bgp_peer(&PeerId::Ip(addr));
+        ctx.db
+            .remove_all_prefixes_from_bgp_peer(&PeerId::Ip(addr), None);
         ctx.db.remove_bgp_neighbor(addr)?;
         get_router!(&ctx, asn)?.delete_session(addr);
 
