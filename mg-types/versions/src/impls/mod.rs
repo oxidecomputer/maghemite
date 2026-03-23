c0cdbdac07127593539675d7727b041e7fa9413c
Zeeshan Lakhani <zeeshan@oxidecomputer.com>
2026-03-23 13:05:18 +0000

    [review] address PR review comments: UnderlayMulticastIpv6 newtype, Vni type, sled transactions, merge main (665)
    This is setup to come after https://github.com/oxidecomputer/maghemite/pull/665's API modifications, but
    uses the up-to-date setup and dropshot API manager.

--- mg-types/versions/src/impls/mod.rs
+++ mg-types/versions/src/impls/mod.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// Copyright 2026 Oxide Computer Company
+
 //! Functional code for the latest versions of types.
 
 mod bgp;
