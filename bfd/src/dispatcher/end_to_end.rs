fcc63d9bde8611967073ffa4c2e8fbde2e06e80b
John Gallagher <john@oxidecomputer.com>
2026-07-27 13:00:41 -0400

    rename bfd-async -> bfd

--- bfd/src/dispatcher/end_to_end.rs
+++ bfd/src/dispatcher/end_to_end.rs
@@ -109,8 +109,8 @@ async fn build_daemon(
 
 #[tokio::test(flavor = "multi_thread")]
 async fn two_sessions_converge_then_peer_loss_drives_down() {
-    let (mut daemon_a, addr_a, db_a) = build_daemon("bfd_async_e2e_a").await;
-    let (mut daemon_b, addr_b, db_b) = build_daemon("bfd_async_e2e_b").await;
+    let (mut daemon_a, addr_a, db_a) = build_daemon("bfd_e2e_a").await;
+    let (mut daemon_b, addr_b, db_b) = build_daemon("bfd_e2e_b").await;
 
     // A's egress targets B's listener and vice versa.
     daemon_a
