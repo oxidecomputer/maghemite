76026503ef26a4b70edd7b849b3cad9d53ae5c53
John Gallagher <john@oxidecomputer.com>
2026-07-27 13:01:11 -0400

    make bfd::PeerInfo private

--- bfd/src/lib.rs
+++ bfd/src/lib.rs
@@ -118,34 +118,34 @@ pub struct SessionCounters {
 
 /// Information about a BFD peer.
 #[derive(Debug, Clone, Copy)]
-pub struct PeerInfo {
+struct PeerInfo {
     /// The interval at which the peer would _like_ to receive BFD control
     /// packets.
-    pub desired_min_tx: Duration,
+    desired_min_tx: Duration,
 
     /// This is the minimum interval between received BFD Control packets that
     /// this system is capable of supporting.
-    pub required_min_rx: Duration,
+    required_min_rx: Duration,
 
     /// A unique identifer for the peer. This structure is used to keep track
     /// of remote peer information as well as our own. The remote peer
     /// generates their own discriminator. When a peer state machine is first
     /// started, we generate our discriminator with
     /// `PeerInfo::with_random_discriminator`
-    pub discriminator: u32,
+    discriminator: u32,
 
     /// Whether or not the peer is requesting demand mode. This means
     /// unsolicited BFD control packets will not be sent. The only control
     /// packets sent will be in response to control packets received with the
     /// poll flag set.
-    pub demand_mode: bool,
+    demand_mode: bool,
 
     /// When multiplied against required_min_rx, defines the detection threshold
     /// connectivity status.
     ///
     /// RFC 5880 §6.8.6, a packet with a detect mult of 0 MUST be discarded; we
     /// encode that here via a `NonZero` type.
-    pub detection_multiplier: NonZeroU8,
+    detection_multiplier: NonZeroU8,
 }
 
 impl Default for PeerInfo {
@@ -164,15 +164,14 @@ impl Default for PeerInfo {
 
 impl PeerInfo {
     /// Initialize a peer info object with a random discriminator.
-    pub fn with_random_discriminator(
+    fn with_random_discriminator(
         required_min_rx: Duration,
         detection_multiplier: NonZeroU8,
     ) -> Self {
         Self {
             required_min_rx,
             detection_multiplier,
-            discriminator: rand::random(),
-            ..Default::default()
+            discriminator: rand::random(), ..Default::default()
         }
     }
 }
